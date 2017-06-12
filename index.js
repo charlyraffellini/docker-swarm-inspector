e69f6e874f79e463c222244c4d3cb76523cca460
Carlos Esteban Raffellini <charlyraffellini@users.noreply.github.com>
2017-06-12 17:57:09 +0100

    remove identation in response message

--- index.js
+++ index.js
@@ -10,18 +10,18 @@ var port = process.env.port || 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function (req, res) {
-    var name = req.query.name || '[Not defined]';
     var connection = req.connection;
     res.status(200);
     res.set('Content-Type', 'text/plain');
     var message = `Request: ${req.headers.host}
-        Local Address: ${connection.localAddress}
-        Local Port: ${connection.localPort}
-        Remote Address: ${connection.remoteAddress}
-        Remote Family: ${connection.remoteFamily}
-        Remote Port: ${connection.remotePort}
-    `;
-     res.send(message);
+Local Address: ${connection.localAddress}
+Local Port: ${connection.localPort}
+Remote Address: ${connection.remoteAddress}
+Remote Family: ${connection.remoteFamily}
+Remote Port: ${connection.remotePort}
+`;
+
+    res.send(message);
 });
 
 console.log('Listening on port ' + port);
